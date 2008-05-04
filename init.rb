fc053ca9607d2ade8e99b8af7ec6a66ad17e9d6a
rpheath <rpheath@gmail.com>
2008-05-04 01:17:03 -0400

    added call to mixin with FormBuilder

--- init.rb
+++ init.rb
@@ -2,3 +2,4 @@ require 'acts_as_lookup'
 
 ActiveRecord::Base.send(:include, RPH::ActsAsLookup)
 ActionView::Base.send(:include, RPH::ActsAsLookup::ViewHelpers)
+ActionView::Helpers::FormBuilder.send(:include, RPH::ActsAsLookup::ViewHelpers::FormBuilder)
